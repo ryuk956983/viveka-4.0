e5b604d3bccc74bdb6b8f2c4c1296876971d0493
RYUK <c47shmah8@gmail.com>
2025-01-26 20:49:40 +0530

    add

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -16,25 +16,25 @@ const Gallery = () => {
     }, 3000);
 
   let GalleryImg = [
-    "./src/images/gallery/1.869a809e67436d8507f2.jpg",
-    "./src/images/gallery/2.cc7c75aada957787230a.jpg",
-    "./src/images/gallery/3.2aa9126e8d3b706dc856.jpg",
-    "./src/images/gallery/4.0431a5cff6cfc886115c.jpg",
-    "./src/images/gallery/5.baa313ca54655db128e4.jpg",
-    "./src/images/gallery/7.51fb4539beef97816de6.jpg",
-    "./src/images/gallery/8.d6548e05b0bc29a6d643.jpg",
-    "./src/images/gallery/9.703011b21c5e304c5170.jpg",
-    "./src/images/gallery/10.6373a1110d9a3c7832d0.jpg",
-    "./src/images/gallery/11.bcb5604c739fb6909ee5.jpg",
-    "./src/images/gallery/12.f1f6921c43ac1e2f25a8.jpg",
-    "./src/images/gallery/14.9d0044bdebfa8e8ab508.jpg",
-    "./src/images/gallery/15.0e768f287ce41f36b34c.jpg",
-    "./src/images/gallery/16.f1f6921c43ac1e2f25a8.jpg",
-    "./src/images/gallery/17.b75a597aad2dd147ee75.jpg",
-    "./src/images/gallery/18.5c18d7424cea909a102c.jpg",
-    "./src/images/gallery/19.19761fc5916f4d0e0c3f.jpg",
-    "./src/images/gallery/20.0d01021ff80ef5ea4da5.jpg",
-    "./src/images/gallery/21.c21bcdee2cbc34194124.jpg"
+    "../src/images/gallery/1.869a809e67436d8507f2.jpg",
+    "../src/images/gallery/2.cc7c75aada957787230a.jpg",
+    "../src/images/gallery/3.2aa9126e8d3b706dc856.jpg",
+    "../src/images/gallery/4.0431a5cff6cfc886115c.jpg",
+    "../src/images/gallery/5.baa313ca54655db128e4.jpg",
+    "../src/images/gallery/7.51fb4539beef97816de6.jpg",
+    "../src/images/gallery/8.d6548e05b0bc29a6d643.jpg",
+    "../src/images/gallery/9.703011b21c5e304c5170.jpg",
+    "../src/images/gallery/10.6373a1110d9a3c7832d0.jpg",
+    "../src/images/gallery/11.bcb5604c739fb6909ee5.jpg",
+    "../src/images/gallery/12.f1f6921c43ac1e2f25a8.jpg",
+    "../src/images/gallery/14.9d0044bdebfa8e8ab508.jpg",
+    "../src/images/gallery/15.0e768f287ce41f36b34c.jpg",
+    "../src/images/gallery/16.f1f6921c43ac1e2f25a8.jpg",
+    "../src/images/gallery/17.b75a597aad2dd147ee75.jpg",
+    "../src/images/gallery/18.5c18d7424cea909a102c.jpg",
+    "../src/images/gallery/19.19761fc5916f4d0e0c3f.jpg",
+    "../src/images/gallery/20.0d01021ff80ef5ea4da5.jpg",
+    "../src/images/gallery/21.c21bcdee2cbc34194124.jpg"
   ];
 
   return (
